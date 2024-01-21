2e87566007bdc03094138edf51e5590ad39d4288
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-01-21 22:35:55 +0530

    commit 5

--- app/page.js
+++ app/page.js
@@ -1,9 +1,35 @@
-import Image from "next/image";
-
+"use client";
 export default function Home() {
+
+    const addGrudge = (e) => {
+      e.preventDefault();
+      // Add your logic to handle the form submission here
+    };
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full p-4 bg-white rounded shadow-md md:p-8 md:w-96">
+        <form onSubmit={addGrudge}>
+          <label
+            htmlFor="grudgeName"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name Of Grudge
+          </label>
+          <input
+            type="text"
+            name="grudgeName"
+            id="grudgeName"
+            placeholder="Name of Grudge"
+            className="w-full p-2 mt-1 border rounded focus:outline-none focus:border-blue-500"
+          />
+          <button
+            type="submit"
+            className="w-full p-2 mt-4 text-white bg-blue-500 rounded md:w-auto hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
+          >
+            Add Grudge
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
