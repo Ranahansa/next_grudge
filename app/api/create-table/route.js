06ecfec305c7675ba707d444afa25afcd621a45f
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-01-21 21:19:14 +0530

    commit 2

---
+++ app/api/create-table/route.js
@@ -0,0 +1,12 @@
+import {sql} from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(_req) {
+    try{
+        const result = await sql`CREATE TABLE Grudges (NAME varchar(255) NOT NULL);`;
+        return NextResponse.json({result}, {status: 200});
+    }catch{
+        return NextResponse.json({result: "Error"}, {status: 500});
+    }
+    
+}
