e2f2ae78ba6eb93ca2454f70cff762e6c5a96a6d
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-01-21 21:27:19 +0530

    commit 3

--- app/api/create-table/route.js
+++ app/api/create-table/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(_req) {
     try{
-        const result = await sql`CREATE TABLE Grudges (NAME varchar(255) NOT NULL);`;
+        const result = await sql`CREATE TABLE Grudges (NAME varchar(255));`;
         return NextResponse.json({result}, {status: 200});
     }catch{
         return NextResponse.json({result: "Error"}, {status: 500});
